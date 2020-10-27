c61397484c013a2743ad8a1032804030d8ad8f27
franisulla <theviolthevioletandthewine@gmail.com>
2020-10-28 00:04:59 +0300

    buggy ex4

---
+++ week11/ex4.c
@@ -0,0 +1,30 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(){
+	int fildes1 = open("ex1.txt", O_RDWR);
+	int fildes2 = open("ex1.memcpy.txt", O_RDWR | O_CREAT);
+	
+	struct stat statbuf;
+	fstat (fildes1, &statbuf);
+	size_t n = statbuf.st_size;
+	
+	ftruncate(fildes2, n);
+
+	char* adr1 = mmap(NULL, n, PROT_READ | PROT_WRITE, MAP_SHARED, fildes1, 0);
+	char* adr2 = mmap(NULL, n, PROT_READ | PROT_WRITE, MAP_SHARED, fildes2, 0);
+
+	memcpy(adr2, adr1, n);
+
+	munmap(adr1, n);
+	munmap(adr2, n);
+
+	close(fildes1);
+	close(fildes2);
+	return 0;
+}
